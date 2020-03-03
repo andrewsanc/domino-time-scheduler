188004ac8dba044526a70209576a40410ed6b00a
andrewsanc <andrewsanc@csumb.edu>
2020-03-03 15:29:27 -0800

    rendering 24 dominos

--- src/Components/App/App.js
+++ src/Components/App/App.js
@@ -5,14 +5,28 @@ import Domino from "../Domino/Domino";
 class App extends Component {
   constructor(props) {
     super(props);
-    this.state = {};
+    this.state = {
+      selected: null
+    };
   }
+
+  onMouseOverHandler = card => {
+    this.setState({ selected: card });
+  };
+
   render() {
+    const dominos = [];
+    for (let i = 0; i < 24; i++) {
+      dominos.push(
+        <Domino key={i} hour={i} onMouseOverHandler={this.onMouseOverHandler} />
+      );
+    }
+
     return (
-      <div className="dominoContainer">
-        <Domino />
-        <Domino />
-        <Domino />
+      <div className="app">
+        <div className="dominoContainer">{dominos}</div>
+        <div>
+        </div>
       </div>
     );
   }
--- src/Components/Domino/Domino.js
+++ src/Components/Domino/Domino.js
@@ -1,18 +1,12 @@
-import React, { Component } from 'react';
-import './Domino.css';
+import React from "react";
+import "./Domino.css";
 
-class Domino extends Component {
-  constructor(props) {
-    super(props);
-    this.state = {};
-  }
-  render() {
-    return (
-      <div className="domino">
-        <h4 className="title">Card </h4>
-      </div>
-    );
-  }
-}
+const Domino = ({ hour, onMouseOverHandler }) => {
+  return (
+    <div className="domino" onMouseOver={() => onMouseOverHandler(hour)}>
+      <h4 className="title">{hour}</h4>
+    </div>
+  );
+};
 
 export default Domino;
